e2dcc440576e78c9279cf69e1b93b96046cbd0ae
wcjohnson <wcjohnson@oigroup.net>
2020-07-24 06:15:30 -0700

    feat: Healthchecks in clusterapp helm chart

--- resources/dev/dev-restart.sh
+++ resources/dev/dev-restart.sh
@@ -2,3 +2,6 @@
 
 kubectl --namespace=development rollout restart deployment/worker-io
 kubectl --namespace=development rollout restart deployment/api
+
+kubectl --namespace=development rollout status deployment/api
+kubectl --namespace=development rollout status deployment/worker-io
